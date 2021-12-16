0a198d8f21729a54dca9c4dc23368db1931f9107
isabeaurikkoert <57255022+isabeaurikkoert@users.noreply.github.com>
2021-12-16 21:42:49 +0100

    spelen tegen computer

--- thonny boterkas.py
+++ thonny boterkas.py
@@ -1,4 +1,11 @@
-from bke import start
+import random
+from bke import EvaluationAgent, start
 
 
-start()
+class MyRandomAgent(EvaluationAgent):
+    def evaluate(self, board, my_symbol, opponent_symbol):
+        return random.randint(1,500)
+    
+    
+my_random_agent = MyRandomAgent()
+start(player_o=my_random_agent)
