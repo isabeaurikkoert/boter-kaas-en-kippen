c6cadce37c8d214431e0b3e1709b101cde1e4df2
isabeaurikkoert <57255022+isabeaurikkoert@users.noreply.github.com>
2021-12-17 18:06:03 +0100

    computer laten oefenen

--- thonny boterkas.py
+++ thonny boterkas.py
@@ -1,11 +1,19 @@
-import random
-from bke import EvaluationAgent, start
-
-
-class MyRandomAgent(EvaluationAgent):
-    def evaluate(self, board, my_symbol, opponent_symbol):
-        return random.randint(1,500)
+from bke import MLAgent, is_winner, opponent, train, save
+ 
+ 
+class MyAgent(MLAgent):
+    def evaluate(self, board):
+        if is_winner(board, self.symbol):
+            reward = 1
+        elif is_winner(board, opponent[self.symbol]):
+            reward = -1
+        else:
+            reward = 0
+        return reward
     
-    
-my_random_agent = MyRandomAgent()
-start(player_o=my_random_agent)
+ 
+my_agent = MyAgent()
+ 
+train(my_agent, 3000)
+ 
+save(my_agent, 'MyAgent_3000')
