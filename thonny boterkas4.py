c61614eac1a6f74bb399bff1e57b02dd49b3b85d
isabeaurikkoert <57255022+isabeaurikkoert@users.noreply.github.com>
2021-12-24 21:59:03 +0100

    uitleg hyperparameters

--- thonny boterkas4.py
+++ thonny boterkas4.py
@@ -21,7 +21,7 @@ my_agent = MyAgent(alpha=0.02, epsilon=0.3)
 
  
    
-print("1: train de computer \n 2: speel tegen een getrainde computer \n 3: je kan tegen ander persoon \n 4: train een AI en kijk hoe goed die het doet \n Kies wat je wilt spelen:")
+print(" 1: train de computer \n 2: speel tegen een getrainde computer \n 3: je kan tegen ander persoon \n 4: train een AI en kijk hoe goed die het doet \n 5: Wat zijn hyperparameters? \n Kies wat je wilt spelen:")
 choice = input()
 
 if choice == '1':
@@ -50,4 +50,7 @@ if choice == '4':
     validation_result = validate(agent_x=my_agent, agent_o=validation_agent, iterations=10000)
      
     plot_validation(validation_result)
+    
+if choice == '5':
+    print("Hyperparameters zijn grenzen die je kan aanpassen. In deze code heb je 2 hyperparameters, alpha en epsilon. De alpha is de leerfactor van de computer. Deze bepaalt hoe snel de computer nieuwe kennis adopteert. Hoe hoger dit getal, hoe sneller de computer geneigd zal zijn om oude kennis te vervangen door nieuwe kennis. De epsilon  is de verkenningsfactor van de computer. Deze bepaalt hoe vaak de computer nieuwe dingen probeert. Hoe hoger dit getal, hoe vaker de computer een willekeurige actie probeert in plaats van de best bekende zet. Door te spelen met deze twee parameters kan je dus een beetje controleren hoe 'slim' de computer is.")
     
